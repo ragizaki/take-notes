100a6942a1a1125e50d4ed540b059e9c45215eb7
Zaki Machfj <zakimachfj@gmail.com>
2022-02-20 15:26:00 -0500

    styled filter bar with mui

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 type ISearchProps = {
     value: string;
@@ -6,10 +8,21 @@ type ISearchProps = {
 };
 
 const SearchBar = ({ value, handleSearch }: ISearchProps): JSX.Element => {
-    console.log('rendered');
     return (
         <div>
-            <input value={value} onChange={handleSearch} type='text' placeholder='Search by title' />
+            <TextField
+                variant='standard'
+                value={value}
+                onChange={handleSearch}
+                label='Filter by title'
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position='start'>
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                }}
+            />
         </div>
     );
 };
